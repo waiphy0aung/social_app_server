d04279380b18f96ae1d64b610d83f94a2e8f8eb0
waiphy0aung <bgyi354@gmail.com>
2023-01-29 10:04:52 +0630

    update

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,4 @@
-import User from "../models/User";
+import User from "../models/User.js";
 
 export const getUser = async (req,res) => {
     try {
@@ -36,5 +36,42 @@ export const getUserFriends =  async (req,res) => {
         })
     } catch (err) {
         res.status(500).json({error: err.message});
+    }
+}
+
+export const addRemoveFriend = async (req,res) => {
+    try {
+        const {id,friendId} = req.body;
+
+        const user = await User.findById(id);
+        const friend = await User.findById(friendId);
+
+        if(user.friends.includes(friendId)){
+            user.friends = user.friends.filter(id => id !== friendId);
+            friend.friends = friend.friends.filter(fid => fid !== id);
+        }else{
+            user.friends.push(friendId);
+            friend.friends.push(id);
+        }
+
+        await user.save();
+        await friend.save();
+
+        const friends = await Promise.all(
+            user.friends.map(id => User.findById(id))
+        )
+
+        const formattedFriends = friends.map(
+            ({_id,firstName,lastName,occupation,picturePath,location}) => {
+                return {_id,firstName,lastName,occupation,picturePath,location}
+            }
+        ) 
+        return res.status(200).json({
+            success: true,
+            payload: formattedFriends,
+            error: null
+        })
+    } catch (err) {
+        res.status(500).json({error : err.message});
     }
 }
--- index.js
+++ index.js
@@ -9,8 +9,11 @@ import morgan from "morgan";
 import path from "path";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
+import postRoutes from "./routes/post.js";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/post.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url);
@@ -40,6 +43,7 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register",upload.single("picture"),register);
+app.post("/posts",verifyToken,upload.single("picture"),createPost);
 
 /* ROUTES */
 app.use("/auth",authRoutes);
