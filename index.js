e4664617bda794d96b2826cc3795c4f695a4f73b
Wai Phyo Aung <waiphyoaung@pishinoMacBook-Air.local>
2023-01-30 20:46:21 +0630

    update

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/post.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import {users,posts} from "./data/index.js"
 
 /* CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url);
@@ -59,5 +62,8 @@ mongoose
         useUnifiedTopology: true,
     }).then(() => {
         app.listen(port,() => console.log(`Server Port: ${port}`));
+
+        // User.insertMany(users);
+        // Post.insertMany(posts);
     })
     .catch(error => console.log(`${error} didn't connect`));
